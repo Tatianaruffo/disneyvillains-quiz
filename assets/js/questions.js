72c77e1a2216bf903954bd6d9af25e5afaa0e8d7
Tatianaruffo <tatiana.ruffo@gmail.com>
2023-03-14 18:07:17 +0000

    Adds questions.js and it's content

---
+++ assets/js/questions.js
@@ -0,0 +1,102 @@
+// Questions for the quiz
+
+const quizQuestions = [{
+    question: "Which Disney Villain has a pet named diablo? ",
+    answers: [
+        {option: 'Cruella'},
+        {option: 'Evil Queen'},
+        {option: 'Maleficent'},
+        {option: 'Ursula'}
+    ],
+    correctAnswer: 'Maleficent'
+},
+{
+    question: "In Cinderella, what is the name of the Tremaine family's black cat?",
+    answers: [
+        {option: "Cat"},
+        {option: "Lucifer"},
+        {option: "Mr. Diablo"},
+        {option: "Evil"}
+    ],
+    correctAnswer: "Lucifer"
+},
+{
+    question: "Which colour is Madam Mim's hair in Sword in the Stone?",
+    answers: [
+        {option: "Purple"},
+        {option: "Blue"},
+        {option: "Pink"},
+        {option: "White"}
+    ],
+    correctAnswer: "Purple"
+},
+{
+    question: "What did Ursula want from Ariel in The Little Mermaid?",
+    answers: [
+        {option: "Her hair"},
+        {option: "Her voice"},
+        {option: "Her eyes"},
+        {option: "Her mind"}
+    ],
+    correctAnswer: "Her voice"
+},
+{
+    question: "Which 90s Disney movie featured a fearsome villain called Frollo?",
+    answers: [
+        {option: "The Hunchback of Notre Dame"},
+        {option: "The Lion King"},
+        {option: "Beauty and the Beast"},
+        {option: "Pocahontas"}
+    ],
+    correctAnswer: "The Hunchback of Notre Dame"
+},
+{
+    question: "Who wants to be the fairest of them all?",
+    answers: [
+        {option: "Ursula"},
+        {option: "Cruella"},
+        {option: "Evil Queen"},
+        {option: "Madame Mim"}
+    ],
+    correctAnswer: "Evil Queen"
+},
+{
+    question: "Who's completely obsessed with fashion and fur?",
+    answers: [
+        {option: "Lady Tremaine"},
+        {option: "Cruella"},
+        {option: "Queen of Hearts"},
+        {option: "Jafar"}
+    ],
+    correctAnswer: "Cruella"
+},
+{
+    question: "Jafar's last wish was to become a..?",
+    answers: [
+        {option: "Sultan"},
+        {option: "Genie"},
+        {option: "Snake"},
+        {option: "Sorcerer"}
+    ],
+    correctAnswer: "Genie"
+},
+{
+    question: "Shan-yu, the villain in Mulan, is the leader of the..",
+    answers: [
+        {option: "Philistines"},
+        {option: "Barbarians"},
+        {option: "Vikings"},
+        {option: "Huns"}
+    ],
+    correctAnswer: "Huns"
+},
+{
+    question: "What was the name of Zeus brother in Hercules?",
+    answers: [
+        {option: "Heron"},
+        {option: "Hades"},
+        {option: "Poseidon"},
+        {option: "Ares"}
+    ],
+    correctAnswer: "Hades"
+}];
