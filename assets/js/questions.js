e3d9be9076e6471dd4daa1d461dde7ea5facc9e8
Tatianaruffo <tatiana.ruffo@gmail.com>
2023-03-15 13:29:44 +0000

    Generates answers from the array with JS

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,100 +3,90 @@
 const questions = [{
     question: "Which Disney Villain has a pet named diablo? ",
     answers: [
-        {option: 'Cruella'},
-        {option: 'Evil Queen'},
-        {option: 'Maleficent'},
-        {option: 'Ursula'}
-    ],
-    correctAnswer: 'Maleficent'
+        {text: 'Cruella', correct: false},
+        {text: 'Evil Queen', correct: false},
+        {text: 'Maleficent', correct: true},
+        {text: 'Ursula', correct: false}
+    ]
 },
 {
     question: "In Cinderella, what is the name of the Tremaine family's black cat?",
     answers: [
-        {option: "Cat"},
-        {option: "Lucifer"},
-        {option: "Mr. Diablo"},
-        {option: "Evil"}
-    ],
-    correctAnswer: "Lucifer"
+        {text: "Cat", correct: false},
+        {text: "Lucifer", correct: true},
+        {text: "Mr. Diablo", correct: false},
+        {text: "Evil", correct: false}
+    ]
 },
 {
     question: "Which colour is Madam Mim's hair in Sword in the Stone?",
     answers: [
-        {option: "Purple"},
-        {option: "Blue"},
-        {option: "Pink"},
-        {option: "White"}
-    ],
-    correctAnswer: "Purple"
+        {text: "Purple", correct: true},
+        {text: "Blue", correct: false},
+        {text: "Pink", correct: false},
+        {text: "White", correct: false}
+    ]
 },
 {
     question: "What did Ursula want from Ariel in The Little Mermaid?",
     answers: [
-        {option: "Her hair"},
-        {option: "Her voice"},
-        {option: "Her eyes"},
-        {option: "Her mind"}
-    ],
-    correctAnswer: "Her voice"
+        {text: "Her hair", correct: false},
+        {text: "Her voice", correct: true},
+        {text: "Her eyes", correct: false},
+        {text: "Her mind", correct: false}
+    ]
 },
 {
     question: "Which 90s Disney movie featured a fearsome villain called Frollo?",
     answers: [
-        {option: "The Hunchback of Notre Dame"},
-        {option: "The Lion King"},
-        {option: "Beauty and the Beast"},
-        {option: "Pocahontas"}
-    ],
-    correctAnswer: "The Hunchback of Notre Dame"
+        {text: "The Hunchback of Notre Dame", correct: true},
+        {text: "The Lion King", correct: false},
+        {text: "Beauty and the Beast", correct: false},
+        {text: "Pocahontas", correct: false}
+    ]
 },
 {
     question: "Who wants to be the fairest of them all?",
     answers: [
-        {option: "Ursula"},
-        {option: "Cruella"},
-        {option: "Evil Queen"},
-        {option: "Madame Mim"}
-    ],
-    correctAnswer: "Evil Queen"
+        {text: "Ursula", correct: false},
+        {text: "Cruella", correct: false},
+        {text: "Evil Queen", correct: true},
+        {text: "Madame Mim", correct: false}
+    ]
 },
 {
     question: "Who's completely obsessed with fashion and fur?",
     answers: [
-        {option: "Lady Tremaine"},
-        {option: "Cruella"},
-        {option: "Queen of Hearts"},
-        {option: "Jafar"}
-    ],
-    correctAnswer: "Cruella"
+        {text: "Lady Tremaine", correct: false},
+        {text: "Cruella", correct: true},
+        {text: "Queen of Hearts", correct: false},
+        {text: "Jafar", correct: false}
+    ]
 },
 {
     question: "Jafar's last wish was to become a..?",
     answers: [
-        {option: "Sultan"},
-        {option: "Genie"},
-        {option: "Snake"},
-        {option: "Sorcerer"}
-    ],
-    correctAnswer: "Genie"
+        {text: "Sultan", correct: false},
+        {text: "Genie", correct: true},
+        {text: "Snake", correct: false},
+        {text: "Sorcerer", correct: false}
+    ]
 },
 {
     question: "Shan-yu, the villain in Mulan, is the leader of the..",
     answers: [
-        {option: "Philistines"},
-        {option: "Barbarians"},
-        {option: "Vikings"},
-        {option: "Huns"}
-    ],
-    correctAnswer: "Huns"
+        {text: "Philistines", correct: false},
+        {text: "Barbarians", correct: false},
+        {text: "Vikings", correct: false},
+        {text: "Huns", correct: true}
+    ]
 },
 {
     question: "What was the name of Zeus brother in Hercules?",
     answers: [
-        {option: "Heron"},
-        {option: "Hades"},
-        {option: "Poseidon"},
-        {option: "Ares"}
-    ],
-    correctAnswer: "Hades"
+        {text: "Heron", correct: false},
+        {text: "Hades", correct: true},
+        {text: "Poseidon", correct: false},
+        {text: "Ares", correct: false}
+    ]
 }];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('btn-start');
+const nextButton = document.getElementById('next');
 
 let shuffledQuestions, currentQuestionsIndex; 
 const questionElement = document.getElementById('question-area');
@@ -16,12 +17,30 @@ function startGame () {
 }
 
 function setNextQuestion () {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionsIndex])
 
 }
 
 function showQuestion(questions) {
     questionElement.innerText = questions.question
+    questions.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn', 'answer-btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 }
 
 function selectAnswer () {
